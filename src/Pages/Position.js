df12e6f1700da38adeb7ca92127d0a048a44e3b8
spenceryang <yangzijian@gmail.com>
2023-10-07 12:26:37 +0800

    position page is now live

---
+++ src/Pages/Position.js
@@ -0,0 +1,42 @@
+import {
+  Box,
+  Center,
+  VStack,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+} from "@chakra-ui/react";
+
+import { useNavigate } from "react-router-dom";
+
+export const Position = () => {
+  const navigate = useNavigate();
+
+  const handleLogIn = () => {
+    // useAuth().login(user);  // Why does this not work?
+    // authUser.login(user);
+    navigate("home");
+  };
+
+  return (
+    <>
+      <Center>
+        <VStack>
+          <div>This is the Login Page</div>
+
+          <InputGroup size="sm">
+            <InputLeftAddon children="Email" />
+            <Input variant="outline" placeholder="Outline" />
+          </InputGroup>
+
+          <InputGroup size="sm">
+            <InputLeftAddon children="Password" />
+            <Input variant="outline" placeholder="Outline" />
+          </InputGroup>
+
+          <input type="button" onClick={handleLogIn} value="Log In" />
+        </VStack>
+      </Center>
+    </>
+  );
+};
