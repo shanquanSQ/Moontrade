c097128c1fe991aa082020a36aa83a625c21530c
shanquanSQ <thiashanquan93@gmail.com>
2023-10-09 17:47:05 +0800

    hardcoded APIs work, still fixing multiple API async calls

--- src/Pages/Trade.js
+++ src/Pages/Trade.js
@@ -13,6 +13,24 @@ import {
 import { useAuth } from "../util/auth";
 import { TradingView } from "../Components/TradingView/TradingView";
 
+// Helper Functions
+function getDatesInMonth(month, year) {
+  const date = new Date(year, month - 1, 1);
+  const days = [];
+  while (date.getMonth() === month - 1) {
+    const addDate = `${new Date(date).getFullYear()}-${new Date(
+      date
+    ).getMonth()}-${String(new Date(date).getDate()).padStart(2, "0")}`;
+
+    days.push(addDate);
+    date.setDate(date.getDate() + 1);
+  }
+  // console.log(days);
+  return days;
+}
+
+getDatesInMonth(1, 2023);
+
 export function Trade() {
   const { Symbol } = useParams();
   const db = getDatabase();
@@ -33,6 +51,86 @@ export function Trade() {
   const userID = auth.user.uid; // This will give you the uid of the logged-in user
 
   const [stockChartData, setStockChartData] = useState([]); // Data for the chart
+
+  useEffect(() => {
+    const daysInfo = getDatesInMonth(9, 2023);
+    console.log("days info is : ", daysInfo);
+    const chartInfo = [];
+    // Month in JavaScript is 0-indexed (January is 0, February is 1, etc),
+    // But I made some modifications so you can key in 1-12 normally, just fyi
+
+    const fetchData = () => {
+      console.log("TRIGGERED");
+      for (const date of daysInfo) {
+        console.log("individual date: ", date);
+        const individualDateData = axios.get(
+          `https://api.polygon.io/v1/open-close/AAPL/${date}?adjusted=true&apiKey=${process.env.REACT_APP_POLYGON_API_KEY}`
+        );
+        Promise.all[individualDateData].then((response) => {
+          const dailyPriceData = response[0].data;
+          const dailyClose = dailyPriceData.close;
+          const priceDataDate = dailyPriceData.from;
+          console.log("response is: ", dailyPriceData);
+          chartInfo.push({ time: priceDataDate, value: dailyClose });
+        });
+      }
+    };
+
+    // const fetchData = async () => {
+    //   for (const date of daysInfo) {
+    //     console.log("info: ", date);
+    //     try {
+    //       console.log("trying");
+    //       const response = await axios.get(
+    //         `https://api.polygon.io/v1/open-close/AAPL/${date}?adjusted=true&apiKey=${process.env.REACT_APP_POLYGON_API_KEY}`
+    //       );
+    //       console.log(`Data for ${date}:`, response.data);
+    //       chartInfo.push(response.data);
+    //     } catch (error) {
+    //       console.error(`Error for ${date}:`, error);
+    //     }
+    //   }
+    // };
+
+    // Promise.all([fetchData]).then(() => {
+    //   console.log("chart info is: ", chartInfo);
+    //   console.log("DONE!");
+    // });
+
+    // Polygon.io API call to get Daily Open/Close data.
+    // Need to run the API call 30 times to get monthly data.
+    const dailyOpenClose = axios.get(
+      `https://api.polygon.io/v1/open-close/AAPL/2023-10-05?adjusted=true&apiKey=${process.env.REACT_APP_POLYGON_API_KEY}`
+    );
+
+    const dailyOpenClose2 = axios.get(
+      `https://api.polygon.io/v1/open-close/AAPL/2023-10-06?adjusted=true&apiKey=${process.env.REACT_APP_POLYGON_API_KEY}`
+    );
+
+    Promise.all([dailyOpenClose, dailyOpenClose2]).then((response) => {
+      const dailyPriceData = response[0].data;
+      const dailyPriceData2 = response[1].data;
+
+      const dailyClose = dailyPriceData.close;
+      const priceDataDate = dailyPriceData.from;
+
+      const dailyClose2 = dailyPriceData2.close;
+      const priceDataDate2 = dailyPriceData2.from;
+
+      console.log(dailyPriceData);
+      console.log(dailyPriceData2);
+
+      // Need to convert the date to a UNIX timestamp.
+      // use JS's Date.parse(date string)
+      // const priceDataDate = Date.parse(dailyPriceData.from);
+      // const priceDataDate2 = Date.parse(dailyPriceData2.from);
+
+      setStockChartData([
+        { time: priceDataDate, value: dailyClose },
+        { time: priceDataDate2, value: dailyClose2 },
+      ]);
+    });
+  }, []);
 
   useEffect(() => {
     if (userID) {
@@ -159,6 +257,7 @@ export function Trade() {
       )}
 
       {/* Your TradingView chart can go here */}
+      {/* {console.log("the stock chart data: ",  stockChartData)} */}
       <TradingView ticker={Symbol} data={stockChartData} />
 
       {/* Remaining code for buying/selling and other functionalities */}
