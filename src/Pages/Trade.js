1f02ede44fa513b9cdce73842e877f71f161da91
shanquanSQ <thiashanquan93@gmail.com>
2023-10-10 09:37:46 +0800

    basic trading view added

--- src/Pages/Trade.js
+++ src/Pages/Trade.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+
 import {
   getDatabase,
   ref,
@@ -14,22 +15,24 @@ import { useAuth } from "../util/auth";
 import { TradingView } from "../Components/TradingView/TradingView";
 
 // Helper Functions
+// Month in JavaScript is 0-indexed (January is 0, February is 1, etc),
+// But I made some modifications so you can key in 1-12 normally, just fyi
 function getDatesInMonth(month, year) {
   const date = new Date(year, month - 1, 1);
   const days = [];
   while (date.getMonth() === month - 1) {
-    const addDate = `${new Date(date).getFullYear()}-${new Date(
-      date
-    ).getMonth()}-${String(new Date(date).getDate()).padStart(2, "0")}`;
+    const addDate = `${new Date(date).getFullYear()}-${String(
+      new Date(date).getMonth()
+    ).padStart(2, "0")}-${String(new Date(date).getDate()).padStart(2, "0")}`;
 
     days.push(addDate);
     date.setDate(date.getDate() + 1);
   }
-  // console.log(days);
+  console.log("all days is: ", days);
   return days;
 }
 
-getDatesInMonth(1, 2023);
+// getDatesInMonth(1, 2023);
 
 export function Trade() {
   const { Symbol } = useParams();
@@ -53,83 +56,41 @@ export function Trade() {
   const [stockChartData, setStockChartData] = useState([]); // Data for the chart
 
   useEffect(() => {
-    const daysInfo = getDatesInMonth(9, 2023);
-    console.log("days info is : ", daysInfo);
-    const chartInfo = [];
-    // Month in JavaScript is 0-indexed (January is 0, February is 1, etc),
-    // But I made some modifications so you can key in 1-12 normally, just fyi
-
-    const fetchData = () => {
-      console.log("TRIGGERED");
-      for (const date of daysInfo) {
-        console.log("individual date: ", date);
-        const individualDateData = axios.get(
-          `https://api.polygon.io/v1/open-close/AAPL/${date}?adjusted=true&apiKey=${process.env.REACT_APP_POLYGON_API_KEY}`
-        );
-        Promise.all[individualDateData].then((response) => {
-          const dailyPriceData = response[0].data;
-          const dailyClose = dailyPriceData.close;
-          const priceDataDate = dailyPriceData.from;
-          console.log("response is: ", dailyPriceData);
-          chartInfo.push({ time: priceDataDate, value: dailyClose });
-        });
-      }
-    };
-
-    // const fetchData = async () => {
-    //   for (const date of daysInfo) {
-    //     console.log("info: ", date);
-    //     try {
-    //       console.log("trying");
-    //       const response = await axios.get(
-    //         `https://api.polygon.io/v1/open-close/AAPL/${date}?adjusted=true&apiKey=${process.env.REACT_APP_POLYGON_API_KEY}`
-    //       );
-    //       console.log(`Data for ${date}:`, response.data);
-    //       chartInfo.push(response.data);
-    //     } catch (error) {
-    //       console.error(`Error for ${date}:`, error);
-    //     }
-    //   }
-    // };
-
-    // Promise.all([fetchData]).then(() => {
-    //   console.log("chart info is: ", chartInfo);
-    //   console.log("DONE!");
-    // });
-
+    /////////////////////////////////////////
     // Polygon.io API call to get Daily Open/Close data.
     // Need to run the API call 30 times to get monthly data.
-    const dailyOpenClose = axios.get(
-      `https://api.polygon.io/v1/open-close/AAPL/2023-10-05?adjusted=true&apiKey=${process.env.REACT_APP_POLYGON_API_KEY}`
-    );
+    ////////////////////////////////////////
 
-    const dailyOpenClose2 = axios.get(
-      `https://api.polygon.io/v1/open-close/AAPL/2023-10-06?adjusted=true&apiKey=${process.env.REACT_APP_POLYGON_API_KEY}`
-    );
+    const daysInfo = getDatesInMonth(9, 2023);
+    // console.log("days info is : ", daysInfo);
+    const chartInfo = [];
 
-    Promise.all([dailyOpenClose, dailyOpenClose2]).then((response) => {
-      const dailyPriceData = response[0].data;
-      const dailyPriceData2 = response[1].data;
+    const getOneDayData = async (eachDate) => {
+      console.log("each date is: ", eachDate);
+      return await axios.get(
+        `https://api.polygon.io/v1/open-close/AAPL/${eachDate.toString()}?adjusted=true&apiKey=${
+          process.env.REACT_APP_POLYGON_API_KEY
+        }`
+      );
+    };
 
-      const dailyClose = dailyPriceData.close;
-      const priceDataDate = dailyPriceData.from;
+    const getAllDaysData = async (alldays) => {
+      for (let i = 0; i < alldays.length; i++) {
+        try {
+          const response = await getOneDayData(alldays[i]);
+          const data = response.data;
 
-      const dailyClose2 = dailyPriceData2.close;
-      const priceDataDate2 = dailyPriceData2.from;
+          chartInfo.push({ time: data.from, value: data.close });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      // This console.log will show the sequence of how the async functions run
+      console.log("chart is: ", chartInfo);
+      setStockChartData(chartInfo);
+    };
 
-      console.log(dailyPriceData);
-      console.log(dailyPriceData2);
-
-      // Need to convert the date to a UNIX timestamp.
-      // use JS's Date.parse(date string)
-      // const priceDataDate = Date.parse(dailyPriceData.from);
-      // const priceDataDate2 = Date.parse(dailyPriceData2.from);
-
-      setStockChartData([
-        { time: priceDataDate, value: dailyClose },
-        { time: priceDataDate2, value: dailyClose2 },
-      ]);
-    });
+    getAllDaysData(daysInfo);
   }, []);
 
   useEffect(() => {
@@ -164,7 +125,7 @@ export function Trade() {
           volume: priceData.q,
           description: detailData.description,
           homepageURL: detailData.homepage_url,
-          logoURL: detailData.branding.logo_url,
+          // logoURL: detailData.branding.logo_url,  //throwing an error
           marketCap: detailData.market_cap,
         });
       })
